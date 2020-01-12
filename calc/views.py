7825fba2095403eb22ee689587ff7ed9c95398e7
charanpapani <charankumar.pcy@gmail.com>
2020-01-12 13:31:04 +0530

    adding a button

--- calc/views.py
+++ calc/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name': 'Charan'})
+    return render(request, 'home.html', {'name': 'Charan  kumar'})
 
 def add(request):
 
